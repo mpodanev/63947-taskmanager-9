bed09904ffad02022f9c88827b72d8a2aa9e9427
Mikhail Podanev <mpodanev@MacBook-Pro-Mikhail.local>
2019-08-14 23:02:31 +0300

    prepeared mock data

--- src/main.js
+++ src/main.js
@@ -2,15 +2,15 @@ import {controlBtnWrap as createControlBtnWrapTemplate} from './components/contr
 import {mainSearch as createMainSearchTemplate} from './components/mainSearchTemplate';
 import {mainFilter as createMainFilterTemplate} from './components/mainFilterTemplate';
 import {board as createBoardTemplate} from './components/boardTemplate';
-
 import {boardFilterList as createBoardFilterList} from './components/boardFilterListTemplate';
 import {createTaskEditTemplate} from './components/taskEditTemplate';
 import {createTask as createTaskTemplate} from './components/taskTemplate';
 import {loadMoreBtn as createLoadMoreBtnTemplate} from './components/loadMoreBtnTemplate';
+import {getTask as taskData} from './components/data';
 
 const mainElement = document.querySelector(`.main`);
 const mainControlElement = document.querySelector(`.main__control`);
-
+const TASK_COUNT = 3;
 const render = (container, element, position) => {
   container.insertAdjacentHTML(position, element);
 };
@@ -26,7 +26,15 @@ const boardTasksElement = document.querySelector(`.board__tasks`);
 render(boardElement, createBoardFilterList(), `afterbegin`);
 render(boardTasksElement, createTaskEditTemplate(), `beforeend`);
 
-new Array(3).fill(``).forEach(() => render(boardTasksElement, createTaskTemplate(), `beforeend`));
+const renderTasks = (container, count) => {
+  container.insertAdjacentHTML(`beforeend`, new Array(count)
+    .fill(``)
+    .map(taskData)
+    .map(createTaskTemplate)
+    .join(``));
+};
+
+renderTasks(boardTasksElement, TASK_COUNT);
 
 render(boardElement, createLoadMoreBtnTemplate(), `beforeend`);
 
